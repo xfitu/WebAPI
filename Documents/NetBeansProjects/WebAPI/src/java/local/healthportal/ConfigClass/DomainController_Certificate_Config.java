5a834cd42113a3d28800885c9357a399669aad2b
julio vaz <gomilari@gmail.com>
2017-05-02 03:08:37 +0700

    Add files via upload

---
+++ Documents/NetBeansProjects/WebAPI/src/java/local/healthportal/ConfigClass/DomainController_Certificate_Config.java
@@ -0,0 +1,20 @@
+
+package local.healthportal.ConfigClass;
+
+/**
+ * This is configuration class for Domain Controller server public key certificate
+ * for SSL or LDAPS connection 
+ * it is that path to certificate of the domain controller where Active Directory exists on
+ * the certificate contains the public key of the domain controller and has already been
+ * imported into the JDK trust store cacerts and new certificate file with the extension JKS
+ * is created at the time of certificate imported, it is a certificate file type which is 
+ * specific and only recognized by JDK trust store in order to make SSL connection between 
+ * the java web application and the Active Directory
+ * @author Julio Vaz
+ */
+public class DomainController_Certificate_Config {
+ 
+    public final static String DOMAIN_CONTROLLER_CERTIFICATE_PATH = "C:\\AD-certificate\\dcad-portal-local.JKS";//1.AD-portal server certificate on virtual box domain controller-->WORK FINE
+    
+}
+
